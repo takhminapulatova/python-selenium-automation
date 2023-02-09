06a9e15fd7238837d5c7bd4afe5b806dd05a3143
takhminapulatova <takhminap@gmail.com>
2023-02-08 22:43:37 -0600

    Chromedriver set up

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path='/Users/tahmina/Desktop/Careerist/python-selenium-automation/chromedriver')
 driver.maximize_window()
 
 # open the url

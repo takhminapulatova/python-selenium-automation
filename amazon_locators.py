521eacec0c1c0570ebf4044b985f6f0222654c45
takhminapulatova <takhminap@gmail.com>
2023-02-09 05:31:53 -0600

    Amazon locators

---
+++ amazon_locators.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+#Amazon logo
+driver.find_element(By.XPATH, "//i [@aria-label = 'Amazon']")
+
+#Continue button
+driver.find_element(By.ID, 'continue')
+
+#Need help link
+driver.find_element(By.XPATH, "//a [@role = 'button']")
+
+#Forgot your password link
+driver.find_element(By.ID, 'auth-fpp-link-bottom')
+
+#Other issues with Sign-In link
+driver.find_element(By.ID, 'ap-other-signin-issues-link')
+
+#Create your Amazon account button
+driver.find_element(By.ID, 'createAccountSubmit')
+
+#*Conditions of use link
+driver.find_element(By.XPATH, "//a [contains(@href, 'notification_condition_of_use')]")
+
+#*Privacy Notice link
+driver.find_element(By.XPATH, "//a [contains(@href, 'notification_privacy_notice')]")
+
---
+++ amazon_sign_in_page_test.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+service = Service('/Users/tahmina/Desktop/Careerist/python-selenium-automation/chromedriver')
+driver = webdriver.Chrome(service=service)
+
+driver.get('https://www.amazon.com/')
+
+driver.find_element(By.ID, 'nav-orders').click()
+expected_result = True
+actual_result = driver.find_element(By.XPATH, "//h1 [@class='a-spacing-small']").is_displayed()
+
+assert expected_result == actual_result, f'Expected -> {expected_result} and actual -> {actual_result}'
+driver.quit()
+
+
